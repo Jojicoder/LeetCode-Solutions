5b6910c40fcb7660d67e7c5d20a9b1ad9af333fa
Jojobone <60705659+Jojikkkkk@users.noreply.github.com>
2025-12-13 11:39:10 -0500

    Time: 0 ms (100%), Space: 8.5 MB (68.56%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -4,18 +4,15 @@ public:
         if(x<0)
             return false;
 
-        
-        string s = to_string(x);
-        string check = s;
-        int n = s.length();
-       
-        string reversed5 = s;
-        for(int i =0; i< n/2;i++){
-            swap(check[i],check[n-i-1]);
-        }
+        int ori = x;
+        long check = 0;
 
-        // ヒント4: 元の s と reversedS が同じなら true, 違えば false
-        if (s == check) {
+        while(x>0){
+            int di = x % 10;
+            check = di + 10 * check;
+            x /= 10; 
+        }
+        if (ori == check) {
             return true;
         } else {
             return false;
