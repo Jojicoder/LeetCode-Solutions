bbe82a5af8db5b943a9bad2fc619c7b48c28cedc
Jojobone <60705659+Jojicoder@users.noreply.github.com>
2025-12-27 14:10:55 -0500

    Time: 4 ms (53.35%), Space: 15 MB (19.87%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,16 +1,26 @@
+#include <vector>
+#include <unordered_map>
+
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target){
-        unordered_map<int,int> numMap;
+    std::vector<int> twoSum(std::vector<int>& nums, int target) {
+        // キー：数字の値、値：そのインデックス
+        std::unordered_map<int, int> map;
 
-        for(int i = 0; i < nums.size();i++){
-            int complement = target - nums[i];
-            if(numMap.find(complement) != numMap.end()){
-                return {numMap[complement],i};
+        for (int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i]; // 必要な相手の数字
+
+            // 相手がマップに存在するか確認 (.count() はキーがあれば1、なければ0を返す)
+            if (map.count(complement)) {
+                // 見つかった場合、{相手のインデックス, 現在のインデックス} を返す
+                return {map[complement], i};
             }
-            numMap[nums[i]] = i;
+
+            // 現在の数字とインデックスをマップに登録
+            map[nums[i]] = i;
         }
 
+        // 解が見つからなかった場合は空のベクターを返す
         return {};
     }
 };
