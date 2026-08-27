627e54228f71dc2294e1d64151f823b665b6e042
Jojicoder <60705659+Jojicoder@users.noreply.github.com>
2026-08-27 12:42:59 -0400

    Time: 0 ms (100%), Space: 8.7 MB (35.91%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
+    
         if(x<0)
             return false;
 
-        int ori = x;
-        long check = 0;
+        int original = x;
+        long reversed = 0;
 
-        while(x>0){
-            int di = x % 10;
-            check = di + 10 * check;
-            x /= 10; 
+        while(x>0)
+        {
+            int digit = x % 10;
+            reversed = reversed * 10 + digit;
+
+            x = x/10;
         }
-        if (ori == check) {
-            return true;
-        } else {
-            return false;
-        }
+
+        return original == reversed;
     }
 };
